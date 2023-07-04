b12746bb3ab36df661bffe053bac51b9a56323a3
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-07-04 20:30:40 +0100

    add lowercase

---
+++ src/lowercase.js
@@ -0,0 +1,29 @@
+// @ts-strict
+
+/**
+ * @template {Readonly<string>} T
+ * @typedef {import('type-fest').IsStringLiteral<T> extends true ? Lowercase<T> : string} LowercaseString<T>
+ */
+
+/**
+ * @description get lowercase string
+ *
+ * @template {Readonly<string>} T
+ *
+ * @param {T} input - input string to capitalize. type should be matched to T
+ *
+ * @returns {LowercaseString<T>}
+ *
+ * @example
+ * uppercase('a') // 'A'
+ *
+ * @example
+ * uppercase('abc') // 'Abc'
+ *
+ * @example
+ * uppercase('') // ''
+ *
+ */
+export function toLowerCase(input) {
+	return /** @type {LowercaseString<T>} */ (input.toLowerCase());
+}
---
+++ src/lowercase.test.ts
@@ -0,0 +1,28 @@
+import { toLowerCase } from './lowercase.js';
+import { describe, it, expect } from 'vitest';
+describe('lowercase test', () => {
+	it('should lowercase one letter', () => {
+		const before = 'A' as const;
+		const lowercased = toLowerCase(before);
+		const expected = 'a' satisfies typeof lowercased;
+		expect(lowercased).toBe(expected);
+	});
+	it('should lowercase a word', () => {
+		const before = 'hEllO' as const;
+		const lowercased = toLowerCase(before);
+		const expected = 'hello' satisfies typeof lowercased;
+		expect(lowercased).toBe(expected);
+	});
+	it('should lowercase a sentence', () => {
+		const before = 'hellO wOrld' as const;
+		const lowercased = toLowerCase(before);
+		const expected = 'hello world' satisfies typeof lowercased;
+		expect(lowercased).toBe(expected);
+	});
+	it('should lowercase empty string', () => {
+		const before = '' as const;
+		const lowercased = toLowerCase(before);
+		const expected = '' satisfies typeof lowercased;
+		expect(lowercased).toBe(expected);
+	});
+});
