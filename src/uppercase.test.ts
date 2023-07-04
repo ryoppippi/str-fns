c5097662a52e1ab110921ca92803e02a5782ccf6
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-07-04 20:21:05 +0100

    fix description

--- src/uppercase.test.ts
+++ src/uppercase.test.ts
@@ -1,25 +1,25 @@
 import { uppercase } from './uppercase.js';
 import { describe, it, expect } from 'vitest';
 describe('uppercase test', () => {
-	it('should uppercase one letter', () => {
+	it('should one uppercase letter', () => {
 		const before = 'a' as const;
 		const uppercased = uppercase<typeof before>(before);
 		const expected = 'A' satisfies typeof uppercased;
 		expect(uppercased).toBe(expected);
 	});
-	it('should uppercase a word', () => {
+	it('should a uppercase word', () => {
 		const before = 'hello' as const;
 		const uppercased = uppercase<typeof before>(before);
 		const expected = 'HELLO' satisfies typeof uppercased;
 		expect(uppercased).toBe(expected);
 	});
-	it('should uppercase a sentence', () => {
+	it('should a uppercase sentence', () => {
 		const before = 'hello world' as const;
 		const uppercased = uppercase<typeof before>(before);
 		const expected = 'HELLO WORLD' satisfies typeof uppercased;
 		expect(uppercased).toBe(expected);
 	});
-	it('should uppercase empty string', () => {
+	it('should empty string', () => {
 		const before = '' as const;
 		const uppercased = uppercase<typeof before>(before);
 		const expected = '' satisfies typeof uppercased;
