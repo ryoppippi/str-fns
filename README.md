c28e026ca9e0b3c19872b86ac06bf1b6df4ad8aa
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-07-06 10:09:12 +0100

    add uncapitalize function

--- README.md
+++ README.md
@@ -20,6 +20,7 @@ Don't worry, I use JSDoc to make it type-safe.
 ## API
 
 - [`capitalize`](src/capitalize.js) - Capitalize the first letter of a string.
+- [`uncapitalize`](src/uncapitalize.js) - Uncapitalize the first letter of a string.
 - [`uppercase`](src/uppercase.js) - Convert a string to uppercase.
 - [`lowercase`](src/lowercase.js) - Convert a string to lowercase.
 
---
+++ src/uncapitalize.js
@@ -0,0 +1,30 @@
+// @ts-strict
+
+/**
+ * @template {Readonly<string>} T
+ * @typedef {import('type-fest').IsStringLiteral<T> extends true ? Uncapitalize<T> : string} UncapitalizedString<T>
+ */
+
+/**
+ * @description uncapitalize first letter of string
+ *
+ * @template {Readonly<string>} T
+ *
+ * @param {T} input - input string to capitalize. type should be matched to T
+ *
+ * @returns {UncapitalizedString<T>}
+ *
+ * @example
+ * uncapitalize('A') // returns 'a'
+ *
+ * @example
+ * uncapitalize('Abc') // returns 'abc'
+ *
+ * @example
+ * uncapitalize('') // returns ''
+ *
+ */
+export function uncapitalize(input) {
+	const [first, ...rest] = input;
+	return /** @type {UncapitalizedString<T>} */ ([first?.toLowerCase(), ...rest].join(''));
+}
