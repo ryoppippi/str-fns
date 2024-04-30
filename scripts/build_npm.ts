952e9981c2d7b448f0d6b9a0f0b02ace57e2fc9b
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-04-30 11:21:40 +0100

    feat: Add npm publishing workflow
    This commit introduces a new GitHub Actions workflow for publishing
    the package to npm. It also includes a new script for building the npm
    package using the Deno dnt tool. The version of the package is passed
    as an argument to the script. The package.json for the npm package is
    generated by the dnt tool during the build process.
    
    The .gitignore file has been updated to ignore the npm directory which
    is created during the build process.
    
    The import_map.json file has been updated to include the Deno dnt tool
    as a dependency.
    
    The deno.json file has been updated to include a new task for building
    the npm package.

---
+++ scripts/build_npm.ts
@@ -0,0 +1,34 @@
+import { build, emptyDir } from "@deno/dnt";
+
+const name = "str-fns";
+const version = Deno.args[0];
+if (!version) {
+  throw new Error("No version argument is specified");
+}
+console.log("*".repeat(80));
+console.log(`${name} ${version}`);
+console.log("*".repeat(80));
+
+await emptyDir("./npm");
+
+await build({
+  typeCheck: false,
+  test: false,
+  entryPoints: ["src/index.ts"],
+  outDir: "./npm",
+  shims: {
+    deno: "dev",
+  },
+  packageManager: "bun",
+  package: {
+    name,
+    version,
+    author: "@ryoppippi",
+    license: "MIT",
+    repository: "ryoppippi/str-fns",
+  },
+});
+
+// post build steps
+Deno.copyFileSync("LICENSE", "npm/LICENSE");
+Deno.copyFileSync("README.md", "npm/README.md");
